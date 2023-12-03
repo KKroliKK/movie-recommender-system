a8b8802c630d5a562d5c80c0d5fb8dee391134c0
KKroliKK <vagin-andrej02@mail.ru>
2023-12-03 22:47:33 +0300

    save best params

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 DATSET_PATH = os.path.join("..", "data", "raw", "ml-100k")
 UDATA_PATH = os.path.join(DATSET_PATH, "u.data")
@@ -6,7 +7,6 @@ UINFO_PATH = os.path.join(DATSET_PATH, "u.info")
 UITEM_PATH = os.path.join(DATSET_PATH, "u.item")
 UGENRE_PATH = os.path.join(DATSET_PATH, "u.genre")
 UUSER_PATH = os.path.join(DATSET_PATH, "u.user")
-
 
 U1BASE_PATH = os.path.join(DATSET_PATH, "u1.base")
 U1TEST_PATH = os.path.join(DATSET_PATH, "u1.test")
@@ -17,3 +17,8 @@ RATINGS_PATH = os.path.join(INTERIM_PATH, "ratings")
 NUM_RATINGS = 100_000
 NUM_USERS = 943
 NUM_MOVIES = 1682
+
+PARAMS_PATH = os.path.join("..", "models", "params.json")
+params = open(PARAMS_PATH)
+params = json.load(params)
+NUM_USERS_COLLABORATIVE = params["num_users"]
